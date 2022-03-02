a3141472a7e25ca3595e66d2caf4b61d883fdee8
EdgarasMon <edgarasxxx@gmail.com>
2022-03-02 10:19:15 +0200

    installed and used 'react-router-dom', created about page

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,23 @@
+import Card from '../components/shared/Card';
+import { Link } from 'react-router-dom';
+
+function AboutPage() {
+	return (
+		<Card>
+			<div className="about">
+				<h1>About This Project</h1>
+				<p>
+					This is React app for leaving feedbacks for a products or
+					services
+				</p>
+				<p>Version: 1.0.0</p>
+
+				<p>
+					<Link to="/">Back to Home</Link>
+				</p>
+			</div>
+		</Card>
+	);
+}
+
+export default AboutPage;
